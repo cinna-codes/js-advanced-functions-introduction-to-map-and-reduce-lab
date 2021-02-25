77f482315ad17a569e2865ab72c5cb8ceb4b7124
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-24 20:06:12 -0500

    commit tree e26e0e8c134e10ef0f714d05033939164dbc17f9

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    const newArr = []
+    for (const int of sourceArray) {
+        newArr.push(-(int))
+    }
+    return newArr
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    const newArr = []
+    for (const int of sourceArray) {
+        newArr.push(int * 2)
+    }
+    return newArr
+}
+
+function mapToSquare(sourceArray) {
+    const newArr = []
+    for (const int of sourceArray) {
+        newArr.push(Math.pow(int, 2))
+    }
+    return newArr
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for (const int of sourceArray) {
+        total += int
+    }
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (const bool of sourceArray) {
+        if (!!bool === false) { return false }
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (const bool of sourceArray) {
+        if (!!bool === true) { return true }
+    } 
+        return false
+}
